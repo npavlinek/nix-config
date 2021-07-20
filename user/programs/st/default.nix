0fcaf0cc44d7f902da9a48e5a055ee998566ac65
Niko Pavlinek <niko.pavlinek@gmail.com>
2021-07-20 20:09:30 +0200

    st: Add anysize patch

--- user/programs/st/default.nix
+++ user/programs/st/default.nix
@@ -7,6 +7,7 @@ in {
       stCustom = super.st.override {
         conf = readFile ./config.h;
         patches = [
+          ./st-anysize-0.8.4.diff
           ./st-scrollback-0.8.4.diff
           ./st-ligatures-scrollback-20200430-0.8.3.diff
         ];
