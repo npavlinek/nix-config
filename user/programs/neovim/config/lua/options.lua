8b9aef8a715cbcc32f280ebda0dbbb0059af6af9
Niko Pavlinek <niko.pavlinek@gmail.com>
2021-07-25 22:31:26 +0200

    neovim: Small tweaks

--- user/programs/neovim/config/lua/options.lua
+++ user/programs/neovim/config/lua/options.lua
@@ -5,7 +5,6 @@ vim.opt.joinspaces = false
 vim.opt.lazyredraw = true
 vim.opt.list = true
 vim.opt.listchars = 'tab:▸ ,eol:¬'
-vim.opt.number = true
 vim.opt.scrolloff = 3
 vim.opt.shortmess:append('I')
 vim.opt.smartcase = true
@@ -13,7 +12,6 @@ vim.opt.spelllang:append {'sl'}
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.textwidth = 80
-vim.opt.wrap = false
 
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
