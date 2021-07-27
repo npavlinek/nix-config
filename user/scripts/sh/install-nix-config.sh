a236a7361c3acfb6a2a27279506635fec0222871
Niko Pavlinek <niko.pavlinek@gmail.com>
2021-07-27 20:29:07 +0200

    Match NixOS and home-manager versions

--- user/scripts/sh/install-nix-config.sh
+++ user/scripts/sh/install-nix-config.sh
@@ -21,7 +21,7 @@ set_up_home_manager() {
   copy_user_config
   printf 'setting up home manager...\n'
   nix-channel --add \
-    https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+    https://github.com/nix-community/home-manager/archive/release-21.05.tar.gz home-manager
   nix-channel --update
   export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
   nix-shell '<home-manager>' -A install
