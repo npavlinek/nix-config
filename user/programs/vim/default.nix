d5d6f0bf4ce04c1d4e21a9058aaae23e166e487c
Niko Pavlinek <niko.pavlinek@gmail.com>
2021-08-20 16:42:33 +0200

    st, vim: update colors

--- user/programs/vim/default.nix
+++ user/programs/vim/default.nix
@@ -1,9 +1,19 @@
 { pkgs, ... }:
 
 let
+  vim-saturnite = pkgs.vimUtils.buildVimPlugin {
+    name = "vim-saturnite";
+    src = pkgs.fetchFromGitHub {
+      owner = "habamax";
+      repo = "vim-saturnite";
+      rev = "d8676a1aabfe471c5341871b5afc788a2d9b8c3a";
+      sha256 = "08gjkw5nkm2fsn31rzgl2q2r884y8mlh0w5ysp3wl0fsypw4vfqc";
+    };
+  };
   myPlugins = with pkgs.vimPlugins; [
     vim-ledger
     vim-nix
+    vim-saturnite
   ];
   myConfig = builtins.readFile ./vimrc;
 in {
