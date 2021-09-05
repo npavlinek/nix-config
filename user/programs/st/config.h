cf4413659015ae69176d7c90a4221dfc4db5dc3b
Niko Pavlinek <niko.pavlinek@gmail.com>
2021-09-05 11:30:57 +0200

    st: Change font

--- user/programs/st/config.h
+++ user/programs/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVu Sans Mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -129,7 +129,7 @@ static const char *colorname[] = {
 unsigned int defaultfg = 256;
 unsigned int defaultbg = 257;
 static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+static unsigned int defaultrcs = 15;
 
 /*
  * Default shape of cursor
